4a71373e656c69694d96ff5cb7d561e54837ba82
rmoremar <rmoremar@emeal.nttdata.com>
2022-03-30 17:00:24 +0200

    main

--- src/main/java/everis/nttdata/nttdatacenters_hibernate_t2_RMM/Contrato.java
+++ src/main/java/everis/nttdata/nttdatacenters_hibernate_t2_RMM/Contrato.java
@@ -14,78 +14,79 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-@Table(name="contratos")
+@Table(name = "contratos")
 public class Contrato {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="codigo_contrato",unique=true,nullable=false)
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "codigo_contrato", unique = true, nullable = false)
 	private long codigo_contrato;
-	
-	@Column(name="fecha_vigencia",unique=false,nullable=false)
+
+	@Column(name = "fecha_vigencia", unique = false, nullable = false)
 	private Date fecha_vigencia;
-	
-	@Column(name="fecha_caducidad",unique=false,nullable=false)
+
+	@Column(name = "fecha_caducidad", unique = false, nullable = false)
 	@Temporal(TemporalType.DATE)
 	private Date fecha_caducidad;
-	
-	@Column(name="precio_mensual",unique=false,nullable=false)
+
+	@Column(name = "precio_mensual", unique = false, nullable = false)
 	@Temporal(TemporalType.DATE)
 	private Double precio_mensual;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "codigo_cliente")
 	private Cliente cliente;
+	
+	public Contrato() {
+		super();
+	}
 
+	public Contrato(long codigo_contrato, Date fecha_vigencia, Date fecha_caducidad, Double precio_mensual) {
+		super();
+		this.codigo_contrato = codigo_contrato;
+		this.fecha_vigencia = fecha_vigencia;
+		this.fecha_caducidad = fecha_caducidad;
+		this.precio_mensual = precio_mensual;
+	}
 
 	public long getCodigo_contrato() {
 		return codigo_contrato;
 	}
 
-
 	public void setCodigo_contrato(long codigo_contrato) {
 		this.codigo_contrato = codigo_contrato;
 	}
-
 
 	public Date getFecha_vigencia() {
 		return fecha_vigencia;
 	}
 
-
 	public void setFecha_vigencia(Date fecha_vigencia) {
 		this.fecha_vigencia = fecha_vigencia;
 	}
-
 
 	public Date getFecha_caducidad() {
 		return fecha_caducidad;
 	}
 
-
 	public void setFecha_caducidad(Date fecha_caducidad) {
 		this.fecha_caducidad = fecha_caducidad;
 	}
-
 
 	public Double getPrecio_mensual() {
 		return precio_mensual;
 	}
 
-
 	public void setPrecio_mensual(Double precio_mensual) {
 		this.precio_mensual = precio_mensual;
 	}
-
 
 	public Cliente getCliente() {
 		return cliente;
 	}
 
-
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
-	
-	
+
 }
--- src/main/java/everis/nttdata/nttdatacenters_hibernate_t2_RMM/main.java
+++ src/main/java/everis/nttdata/nttdatacenters_hibernate_t2_RMM/main.java
@@ -1,6 +1,7 @@
 package everis.nttdata.nttdatacenters_hibernate_t2_RMM;
 
 import java.sql.DriverManager;
+import java.util.Date;
 
 import org.hibernate.Session;
 
@@ -18,6 +19,11 @@ public class main {
 		ContratoImplementacion contratoimpl = new ContratoImplementacion(session);
 		
 		Cliente cliente = new Cliente(1,"José","Ortega","Ramos","12345678A");
+		
+		Contrato contrato = new Contrato(1,new Date(),new Date(),202.5);
+		contrato.setCliente(cliente);
+		
+		//List<Contrato> contratos = new List();
 	}
 
 }
